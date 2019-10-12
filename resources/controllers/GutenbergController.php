d5c93b2f5514fa5948dda1fbacf78cd7414befc3
Terence O'Donoghue <terence@creativelittledots.co.uk>
2019-10-12 12:38:42 +0100

    Update GutenbergController.php

--- resources/controllers/GutenbergController.php
+++ resources/controllers/GutenbergController.php
@@ -12,23 +12,6 @@
 		
 		public function beforeFilter(Input $request) {
 			
-			filter( 'site_url', function( $url ) {
-
-				return str_replace( get_option( 'siteurl' ) . "wp-login.php?action=rp&", get_option( 'homeurl' ) . "reset-password?", $url );
-				
-			});
-			
-			
-			
-			filter( 'upload_mimes', function($types) {
-				
-				$types['json'] = 'application/json';
-				$types['svg'] = 'image/svg+xml';
-				
-				return $types;
-				
-			}, 1);
-			
 			action('after_switch_theme', function() {
 			
 				(new Database())->activate_gutes_array_save();
