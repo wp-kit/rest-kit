c42584a6b4dbade43e75d02d653dabee93dc61e8
Terence O'Donoghue <terence@creativelittledots.co.uk>
2019-10-14 00:28:17 +0100

    Update routes.php

--- resources/routes.php
+++ resources/routes.php
@@ -2,11 +2,11 @@
 
 	if( defined('GUTENBERG_OBJECT_PLUGIN_CPTS') ) {
 
-		invoke( 'GutenbergController', 'admin_init' );
+		invoke( 'GutenbergController', 'init' );
 		
 	}
 
-	invoke( 'AppController', 'admin_init' );
+	invoke( 'AppController', 'init' );
 	invoke( 'ApiController', 'rest_api_init' );
 	
 	rest_route( 'wp/v2', '/menu', array(Theme\Controllers\ApiController::class, 'getMenu') );
