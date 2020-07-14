ec7c467cdb0d86a702a1cab8792e9a46e07545e9
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-07-15 00:25:58 +0100

    acf key and repeater fix

--- resources/controllers/GutenbergController.php
+++ resources/controllers/GutenbergController.php
@@ -31,7 +31,87 @@
 				
 			});
 			
+			filter('gutenberg_object_plugin_data', function($blocks) {
+				
+				foreach($blocks as $block) {
+					
+					if(strpos($block->name, 'acf/') === 0) {
+						
+						$data = &$block->data->attributes->data;
+						
+						$this->handleBlockData($data);
+						
+					}
+					
+				}
+				
+				return $blocks;
+				
+			});
+			
 			parent::beforeFilter($request);
+			
+		}
+		
+		protected function handleBlockData(&$data) {
+			
+			$data = (array) $data;
+			
+			foreach($data as $key => &$value) {
+				
+				if(is_object($value)) {
+					
+					$value = (array) $value;
+					
+				}
+							
+				if(strpos($key, 'field_') === 0) {
+					
+					$field = (object) get_field_object($key);
+					
+					$data[$field->name] = $data[$key];
+					unset($data[$key]);
+					
+					if($field->type == 'repeater') {
+						
+						$data[$field->name] = array_values($value);
+						
+					}
+					
+				}
+				
+				if(is_array($value)) {
+					
+					$this->handleBlockData($value);
+					
+				} else if(strpos($value, 'field_') === 0 && strpos($key, '_') === 0 && is_numeric($data[substr($key, 1)]) && ((object) get_field_object($value))->type == 'repeater') {
+				
+					$k = substr($key, 1);
+					$length = $data[$k];
+					$data[$k] = [];
+					
+					unset($data[$key]);
+					
+					$keys = array_filter(array_keys($data), function($key) use($k) {
+						return strpos($key, "{$k}_") === 0;	
+					});
+					
+					foreach($keys as $key) {
+						
+						list($i, $prop) = explode('_', str_replace("{$k}_", '', $key));
+						
+						$data[$k][$i] = [
+							$prop => $data[$key]
+						];
+						
+						unset($data[$key]);
+						unset($data["_{$key}"]);
+						
+					}
+					
+				}
+				
+			}
 			
 		}
 		
