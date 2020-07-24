692bfcad50fc05788a6cdc031767190ad4ca751f
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-07-24 19:23:10 +0100

    Update AppController.php

--- resources/controllers/AppController.php
+++ resources/controllers/AppController.php
@@ -16,18 +16,6 @@
 	     */
 		public function boot(Request $request) {
 			
-			filter( 'block_categories', function($categories, $post) {
-				return array_merge(
-					$categories,
-					array(
-						array(
-							'slug' => 'rest-kit-example-blocks',
-							'title' => 'Rest Kit Examples',
-						),
-					)
-				);
-			}, 10, 2);
-			
 			filter( 'site_url', function( $url ) {
 
 				return str_replace( get_option( 'siteurl' ) . "wp-login.php?action=rp&", get_option( 'homeurl' ) . "reset-password?", $url );
@@ -69,17 +57,6 @@
 			];
 			
 			return parent::getScripts();
-			
-		}
-		
-		/**
-	     * Output block HTML
-	     *
-	     * @return string
-	     */
-		public static function renderBlock($block, $inner_blocks) {
-			
-			echo view('blocks.' . $block['name'], compact('block', 'inner_blocks'));
 			
 		}
 		
