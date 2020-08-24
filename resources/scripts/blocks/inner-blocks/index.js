04eef6c85b8efcf5fd93e70373fa480e292178ea
Terence O'Donoghue <terence@creativelittledots.co.uk>
2020-08-24 22:19:31 +0100

    Update index.js

--- resources/scripts/blocks/inner-blocks/index.js
+++ resources/scripts/blocks/inner-blocks/index.js
@@ -6,15 +6,21 @@ import contenteditable from './contenteditable'
 if(window.acf) {
 	
 	const { addFilter } = wp.hooks
-
-	acf.data.blockTypes.filter(block => block.has_inner_blocks).forEach(block => {
-		addFilter("editor.BlockEdit", `with-inner-blocks/${block.name}`, editWithInnerBlocks(block.name, block.inner_block_params))
-		addFilter("blocks.getSaveElement",  `with-inner-blocks/${block.name}`, saveWithInnerBlocks(block.name))
-		acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => moveInnerBlocks(preview, attributes, block) )
-	})
 	
-	acf.data.blockTypes.forEach(block => {
-		acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => contenteditable(preview, attributes, block) )
+	acf.addAction('prepare', () => {
+	
+		acf.data.blockTypes.filter(block => block.has_inner_blocks).forEach(block => {
+			addFilter("editor.BlockEdit", `with-inner-blocks/${block.name}`, editWithInnerBlocks(block.name, block.inner_block_params))
+			addFilter("blocks.getSaveElement",  `with-inner-blocks/${block.name}`, saveWithInnerBlocks(block.name))
+			acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => moveInnerBlocks(preview, attributes, block) )
+		})
+
+		acf.data.blockTypes.forEach(block => {
+			acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => contenteditable(preview, attributes, block) )
+		})
+
 	})
+
+	
 
 }
