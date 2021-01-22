1e6f76e4a148cc4360d0714f0cc8ed9867e35c8c
Terence O'Donoghue <terence@creativelittledots.co.uk>
2021-01-22 00:53:40 +0000

    Update ApiController.php

--- resources/controllers/ApiController.php
+++ resources/controllers/ApiController.php
@@ -14,19 +14,25 @@
 	     */
 		public function boot(Request $request) {
 			
+			/* Add Gutenberg Blocks field as gblocks */
+			if ( ! function_exists( 'use_block_editor_for_post_type' ) ) {
+				require ABSPATH . 'wp-admin/includes/post.php';
+			}
+			
 			$post_types = get_post_types_by_support( [ 'editor' ] );
 			foreach ( $post_types as $post_type ) {
 				if ( use_block_editor_for_post_type( $post_type ) ) {
 					register_rest_field(
 						$post_type,
-						'blocks',
+						'gblocks',
 						['get_callback' => function ( array $post ) {
-							return $this->transform( json_decode( json_encode( parse_blocks( $post['content']['raw'] ) ) ) );
+							return apply_filters( 'rest_response_parse_blocks', json_decode( json_encode( parse_blocks( $post['content']['raw'] ) ) ), $post, $post_type );
 						}]
 					);
 				}
 			}
 			
+			/* Example, add field */
 			register_rest_field(
 				['test', 'post'],
 				'featured_image_url',
@@ -37,83 +43,10 @@
 				]
 			);
 			
-			filter('rest_prepare_page', function($data) {
-				if(!is_admin()) {
-					unset($data->data['content']);
-				}
-				return $data;
-			});
-			
 		}
 		
-		/**
-		 * Get the value of the "featured_image_url" field
-		 *
-		 * @param array $object Details of current post.
-		 * @param string $field_name Name of field.
-		 * @param WP_REST_Request $request Current request
-		 *
-		 * @return mixed
-		 */
 		public function getFeaturedImageUrl( $object, $field_name, $request ) {
 			$thumbnail_id = get_post_thumbnail_id( $object->ID );
 			return wp_get_attachment_image_url($thumbnail_id, 'large');
 		}
-		
-		public static function getMenu($request) {
-			
-			$menu = [];
-
-            foreach(get_nav_menu_items_by_location($request->get_param('menu')) as $item) {
-
-                $item->path = parse_url($item->url, PHP_URL_PATH);
-
-                array_push($menu, $item);
-            }
-
-            return $menu;
-			
-		}
-		
-		protected function transform( $blocks ) {
-	
-			foreach($blocks as &$block) {
-				
-				$block->innerText = trim(preg_replace('/\s\s+/', ' ', strip_tags( $block->innerHTML )));
-				
-				if(strpos($block->blockName, 'acf/') === 0) {
-				
-					acf_setup_meta( json_decode(json_encode($block->attrs->data), true), $block->attrs->id, true );
-					
-					$id = $block->attrs->id;
-					
-					unset($block->attrs->name);
-					unset($block->attrs->id);
-					unset($block->attrs->data);
-					
-					$block = (object) [
-						'name' => $block->blockName,
-						'innerBlocks' => $block->innerBlocks,
-						'innerText' => $block->innerText,
-						'data' => (object) array_merge((array) $block->attrs, get_fields($id))
-					];
-				}
-				else {
-					$block = (object) [
-						'name' => $block->blockName,
-						'innerBlocks' => $block->innerBlocks,
-						'innerText' => $block->innerText,
-						'data' => $block->attrs
-					];
-				}
-				
-				if(!empty($block->innerBlocks) && is_array($block->innerBlocks)) {
-					$block->innerBlocks = $this->transform($block->innerBlocks);
-				}
-			}
-			
-			return array_values(array_filter($blocks, fn($block) => $block->name));
-			
-		}
-		
 	}
