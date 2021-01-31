a4e36c097525a8f4ace8653b83ef4663b2fcb18c
Terence O'Donoghue <terence@creativelittledots.co.uk>
2021-01-31 22:38:23 +0000

    Update contenteditable.js

--- resources/scripts/blocks/inner-blocks/contenteditable.js
+++ resources/scripts/blocks/inner-blocks/contenteditable.js
@@ -5,14 +5,29 @@ export default ($preview, attributes, block) => {
 	const getInput = (el) => {
 		
 		let attribute = el.getAttribute('data-attribute')
+		let parentRepeater = el.closest('[data-repeater]')
+		let index = parentRepeater && parseInt(parentRepeater.getAttribute('data-repeater'))
+		
+		let selector = index ? `.acf-block-fields .acf-row[data-id="row-${index-1}"] .acf-field[data-name="${attribute}"]` : `.acf-block-fields .acf-field[data-name="${attribute}"]`
 			
-		let field = document.querySelector(`.acf-block-fields .acf-field[data-name="${attribute}"]`)
+		let field = document.querySelector(selector)
 		
 		if( field ) {
 		
 			let key = field.getAttribute('data-key')
 			
-			return field.querySelector(`[name="acf-${attributes.id}[${key}]"]`)
+			if(index) {
+				
+				let repeater = field.closest('[data-type="repeater"]')
+				let repeaterKey = repeater.getAttribute('data-key')
+				
+				return field.querySelector(`[name="acf-${attributes.id}[${repeaterKey}][row-${index-1}][${key}]"]`)
+				
+			} else {
+				
+				return field.querySelector(`[name="acf-${attributes.id}[${key}]"]`)
+				
+			}
 			
 		}
 		
