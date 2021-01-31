12c691e8d46608add49c1a7bcca38de776539d19
Terence O'Donoghue <terence@creativelittledots.co.uk>
2021-01-31 22:37:52 +0000

    Update index.js

--- resources/scripts/blocks/inner-blocks/index.js
+++ resources/scripts/blocks/inner-blocks/index.js
@@ -1,6 +1,3 @@
-import editWithInnerBlocks from './editWithInnerBlocks'
-import saveWithInnerBlocks from './saveWithInnerBlocks'
-import moveInnerBlocks from './moveInnerBlocks';
 import contenteditable from './contenteditable'
 
 if(window.acf) {
@@ -8,12 +5,6 @@ if(window.acf) {
 	const { addFilter } = wp.hooks
 	
 	acf.addAction('prepare', () => {
-	
-		acf.data.blockTypes.filter(block => block.has_inner_blocks).forEach(block => {
-			addFilter("editor.BlockEdit", `with-inner-blocks/${block.name}`, editWithInnerBlocks(block.name, block.inner_block_params))
-			addFilter("blocks.getSaveElement",  `with-inner-blocks/${block.name}`, saveWithInnerBlocks(block.name))
-			acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => moveInnerBlocks(preview, attributes, block) )
-		})
 
 		acf.data.blockTypes.forEach(block => {
 			acf.addAction( `render_block_preview/type=${block.name.replace('acf/', '')}`, (preview, attributes) => contenteditable(preview, attributes, block) )
